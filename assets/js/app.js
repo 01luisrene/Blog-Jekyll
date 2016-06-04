19b4521e6596c52af3ccbc749ba15c93328ece70
Luis Rene Mas Mas <01luisrene@gmail.com>
2016-06-04 05:12:46 -0500

    subiendo app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,8 +7,6 @@ requirejs.config({
 													'https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min',
 													'vendor/jquery'
 												],
-			headroom 					: 'vendor/headroom.min',
-			jqueryHeadroom		: 'vendor/jQuery.headroom',
 			ghostHunter				:'vendor/jquery.ghostHunter.min',
 			methods						: 'app/methods.min'
 		}
